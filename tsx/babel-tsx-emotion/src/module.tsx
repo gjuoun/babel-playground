6f7cb2d49f565b690a1af80f7dfac9e3c30b0a4b
Jun Guo <gjuoun@gmail.com>
2022-12-09 05:53:42 -0400

    working on new

--- tsx/babel-tsx-emotion/src/module.tsx
+++ tsx/babel-tsx-emotion/src/module.tsx
@@ -1,4 +1,4 @@
-// import { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 
 export const Component = (props: PropsWithChildren<any>) => {
   return <div>{props.children}</div>;
